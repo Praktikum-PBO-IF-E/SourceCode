80f0b5df0e3840e054d367203c946f6d38452e97
Mahasiswa PC-14 <123210131@student.upnyk.ac.id>
2024-03-07 10:01:56 +0700

    Pertemuan 3

---
+++ src/pertemuan3/GUI.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan3;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.*;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class GUI extends JFrame{
+    JTextField fnama = new JTextField();
+    JLabel lnama = new JLabel("Nama Lengkap : ");
+    
+    JLabel ljk = new JLabel("Jenis Kelamin : ");
+    JRadioButton rbPria = new JRadioButton("Laki-Laki");
+    JRadioButton rbWanita = new JRadioButton("Perempuan");
+    
+    JLabel lagama = new JLabel("Agama");
+    String[] namaAgama = 
+    {"Islam", "Kristen", "Katolik", "Hindu", "Budha", "Konghucu"};
+    JComboBox cmAgama = new JComboBox(namaAgama);
+    
+    JLabel lhobi = new JLabel("Hobi");
+    JCheckBox cbSepakBola = new JCheckBox("Sepak Bola");
+    JCheckBox cbBasket = new JCheckBox("Basket");
+    JCheckBox cbRenang = new JCheckBox("Renang");
+    
+    JButton btSave = new JButton("Save");
+    
+    public GUI(){
+        setTitle("GUI");
+        setDefaultCloseOperation(3);
+        setSize(350,200);
+        
+        ButtonGroup group = new ButtonGroup();
+        group.add(rbPria);
+        group.add(rbWanita);
+        
+//        setLayout(null);
+//        add(lnama);
+//        add(fnama);
+//        add(ljk);
+//        add(rbPria);
+//        add(rbWanita);
+//        add(lagama);
+//        add(cmAgama);
+//        add(lhobi);
+//        add(cbSepakBola);
+//        add(cbBasket);
+//        add(cbRenang);
+//        add(btSave);
+        
+        //setBounds(x,y,o,p)
+        //x sumbu x
+        //y sumbu y
+        //o nilai panjang
+        //p nilai tinggi
+        
+//        lnama.setBounds(10,10, 120, 20);
+//        fnama.setBounds(130,10, 150, 20);
+//        ljk.setBounds(10, 35, 120,20);
+//        rbPria.setBounds(130, 35, 100,20);
+//        rbWanita.setBounds(230, 35, 100,20);
+//        lagama.setBounds(10, 60, 150,20);
+//        cmAgama.setBounds(130, 60, 150,20);
+//        lhobi.setBounds(10, 85, 120,20);
+//        cbSepakBola.setBounds(130, 85, 70,20);
+//        cbBasket.setBounds(200, 85, 70,20);
+//        cbRenang.setBounds(270, 85, 70,20);
+//        btSave.setBounds(200,130,120,20);
+        
+        setLayout(new FlowLayout(FlowLayout.TRAILING));
+        add(lnama);
+        add(btSave);
+        add(ljk);
+        add(cbSepakBola);
+        add(lhobi);
+        
+        setVisible(true);
+    }
+    
+}
---
+++ src/pertemuan3/Pertemuan3.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan3;
+
+import java.util.Scanner;
+import javax.swing.*;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class Pertemuan3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        
+        
+//        JFrame frame = new JFrame("SWING");
+//        frame.setSize(300,200);
+//        frame.setVisible(true);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        
+        
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Masukan bilangan = ");
+//        int testing = Integer.parseInt(sc.next());
+//        System.out.println("Nilai yg dimasukkan = "+ testing);
+//          try{
+//             Scanner sc = new Scanner(System.in);
+//            System.out.print("Masukan bilangan = ");
+//            int testing = Integer.parseInt(sc.next());
+//            System.out.println("Nilai yg dimasukkan = "+ testing);
+//          } catch (Exception e){
+//              System.out.println("TERJADI KESALAHAN");
+//              System.out.println(e.getCause());
+//              System.out.println("PESAN : ");
+//              System.out.println(e.getMessage());
+//          } finally {
+//              System.out.println("TERIMA KASIH");
+//          }
+    }
+    
+}
